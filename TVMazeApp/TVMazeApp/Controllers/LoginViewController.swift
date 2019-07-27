d0cf58fe2d4c529a963b7dfe48b99075ff1cb3d1
devcarlos <carlos.alcala@me.com>
2019-07-27 02:39:58 -0400

    Implemented Settings and Login Functionality

---
+++ TVMazeApp/TVMazeApp/Controllers/LoginViewController.swift
@@ -0,0 +1,128 @@
+//
+//  LoginViewController.swift
+//  TVMazeApp
+//
+//  Created by Carlos Alcala on 7/27/19.
+//  Copyright © 2019 Carlos Alcala. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class LoginViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+    var isTouchEnabled:Bool = false
+    var userPIN = ""
+    
+    var context = LAContext()
+    
+    enum AuthenticationState {
+        case loggedin, loggedout
+    }
+    
+    // authentication state.
+    var state = AuthenticationState.loggedout {
+        
+        // Update the UI on a change.
+        didSet {
+            if state == .loggedin {
+               showHome()
+            }
+        }
+    }
+
+    @IBOutlet weak var pinLabel: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSettings()
+        self.hideKeyboardWhenTappedAround()
+        
+        context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil)
+        
+        // initial app state
+        state = .loggedout
+    }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        login()
+    }
+    
+    func login() {
+        if isTouchEnabled {
+            checkAuthentication()
+        } else {
+            if userPIN == pinLabel.text {
+                showHome()
+            } else {
+                showError(message: "The PIN is wrong. Please try again")
+            }
+        }
+    }
+    
+    func showError(message: String) {
+        // show error
+        let controller = UIAlertController(title: "Login Error", message: message, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+        self.present(controller, animated: true, completion: nil)
+    }
+
+    func setupSettings() {
+        isTouchEnabled = defaults.bool(forKey: "UseTouchID")
+        userPIN = defaults.string(forKey: "UsePIN") ?? ""
+    }
+    
+    func showHome() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+        self.present(vc, animated: true)
+    }
+
+    // check login state
+    func checkAuthentication() {
+        
+        if state == .loggedin {
+            // Log out immediately.
+            state = .loggedout
+            
+        } else {
+            context = LAContext()
+            context.localizedCancelTitle = "Enter PIN"
+            
+            // First check if we have the needed hardware support.
+            var error: NSError?
+            if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+                
+                let reason = "Log in to your account"
+                context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason ) { success, error in
+                    
+                    if success {
+                        
+                        // Move to the main thread because a state update triggers UI changes.
+                        DispatchQueue.main.async { [unowned self] in
+                            self.state = .loggedin
+                        }
+                        
+                    } else {
+                        print(error?.localizedDescription ?? "Failed to authenticate")
+                        
+                        self.showError(message: "Failed to authenticate")
+                        
+                        // Fall back to a asking for username and password.
+                        // ...
+                    }
+                }
+            } else {
+                print(error?.localizedDescription ?? "Can't evaluate policy")
+                
+                self.showError(message: "Failed to authenticate")
+                // Fall back to a asking for username and password.
+                // ...
+            }
+        }
+    }
+}
+
+
--- TVMazeApp/TVMazeApp/Controllers/SettingsViewController.swift
+++ TVMazeApp/TVMazeApp/Controllers/SettingsViewController.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBOutlet weak var pinLabel: UITextField!
+    @IBOutlet weak var touchIDSwitch: UISwitch!
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.loadSettings()
+        self.hideKeyboardWhenTappedAround()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveAction(_ sender: UIButton) {
+        saveSettings()
     }
-    */
-
+    
+    func saveSettings() {
+        defaults.set(touchIDSwitch.isOn, forKey: "UseTouchID")
+        defaults.set(pinLabel.text, forKey: "UsePIN")
+    }
+    
+    func loadSettings() {
+        touchIDSwitch.isOn = defaults.bool(forKey: "UseTouchID")
+        pinLabel.text = defaults.string(forKey: "UsePIN")
+    }
+    
 }
